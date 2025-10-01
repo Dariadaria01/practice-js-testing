3956da84a8010fbd4a7356e96c60158bc2e68e57
Daria Seńko <twoj@email>
2025-10-01 14:36:16 +0200

    test 1

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,8 @@
 import randomNumber from './app';
+
+test('jeśli losujesz liczbę z przedzialu od 1 do 1, to funkcja zwróci 1', () => {
+  const min = 1;
+  const max = 1;
+  const result = randomNumber(min, max);
+  expect(result).toBe(1);
+});
