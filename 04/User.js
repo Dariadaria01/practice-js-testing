a32ecf1705d4a78f6e6daef039e6880a30ef8392
Daria Seńko <twoj@email>
2025-10-01 15:35:15 +0200

    04

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,22 @@
 export default class User {
+  constructor({ email, password }) {
+    if (!email.includes('@') || !email.includes('.')) {
+      throw new Error('Niepoprawny email');
+    }
+    if (password.length < 6) {
+      throw new Error('Niepoprawne hasło');
+    }
+    this.email = email;
+    this.password = password;
+  }
 
+  getEmail() {
+    return this.email;
+  }
+  getPassword() {
+    return this.password;
+  }
+  login() {
+    return this.email.includes('devmentor.pl');
+  }
 }
