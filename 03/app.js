8c6eb1a16fa3f4f0d1f06fd70756c6788697b578
Daria Seńko <twoj@email>
2025-10-01 15:02:11 +0200

    refactor

--- 03/app.js
+++ 03/app.js
@@ -1,13 +1,13 @@
 export default function randomNumber(min, max) {
-  if (min === 1 && max === 1) {
-    return 1;
-  }
-
   if (typeof min !== 'number' || typeof max !== 'number') {
     throw new Error('Argumenty muszą być liczbami');
   }
   if (min > max) {
     throw new Error('Minimalna wartość nie może być większa niż maksymalna');
   }
+  if (min === max) {
+    return min;
+  }
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
